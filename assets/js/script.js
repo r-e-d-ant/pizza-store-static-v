00d0828fc91995602e5206a42e63f1c1b5d16698
r-e-d-ant <mugishathierry65@gmail.com>
2022-06-23 07:25:53 +0200

    some changes

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,7 @@
 
-// add lazy loading on all images
-const images = document.getElementsByClassName('image');
+// // add lazy loading on all images
+// const images = document.getElementsByClassName('image');
 
-for(var i = 0; i < images.length; i++) {
-    console.log(images[i].setAttribute('loading', 'lazy'));
-}
+// for(var i = 0; i < images.length; i++) {
+//     console.log(images[i].setAttribute('loading', 'lazy'));
+// }
